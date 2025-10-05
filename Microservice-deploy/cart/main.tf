f65f4eb3b913dd351bfd6439772465842c238457
Aarti chaple <aartichaple2124@gmail.com>
2025-10-05 17:18:05 +0530

    test

---
+++ Microservice-deploy/cart/main.tf
@@ -0,0 +1,87 @@
+# Namespace (make sure roboshop exists, else Terraform will create it)
+resource "kubernetes_namespace" "roboshop" {
+  metadata {
+    name = "roboshop"
+  }
+}
+
+# ConfigMap
+resource "kubernetes_config_map" "cart" {
+  metadata {
+    name      = "cart"
+    namespace = kubernetes_namespace.roboshop.metadata[0].name
+  }
+
+  data = {
+    REDIS_HOST     = "10.0.0.10"
+    CATALOGUE_HOST = "catalogue"
+    CATALOGUE_PORT = "8080"
+  }
+}
+
+# Deployment
+resource "kubernetes_deployment" "cart" {
+  metadata {
+    name      = "cart"
+    namespace = kubernetes_namespace.roboshop.metadata[0].name
+    labels = {
+      app = "cart"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "cart"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "cart"
+        }
+      }
+
+      spec {
+        container {
+          name  = "cart"
+          image = "public.ecr.aws/w8x4g9h7/roboshop-v3/cart"
+
+          port {
+            container_port = 80
+          }
+
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.cart.metadata[0].name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service
+resource "kubernetes_service" "cart" {
+  metadata {
+    name      = "cart"
+    namespace = kubernetes_namespace.roboshop.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "cart"
+    }
+
+    port {
+      port        = 8080
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
