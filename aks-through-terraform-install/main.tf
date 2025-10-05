a09d6c805cbe8fa6b6b80620c27a26466dd0edec
Aarti chaple <aartichaple2124@gmail.com>
2025-10-05 15:13:11 +0530

    test

---
+++ aks-through-terraform-install/main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+
+resource "azurerm_resource_group" "RG" {
+  name     = "RG"
+  location = "ukwest"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+  dns_prefix          = "akscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.raw_kube_config
+  sensitive = true
+}
