2c8ef2ea44b0a8bc5c47f8e5bcb4e99e55c83fe6
Aarti chaple <aartichaple2124@gmail.com>
2025-10-05 16:14:14 +0530

    test

--- azureCLI.sh
+++ azureCLI.sh
@@ -33,7 +33,7 @@ az login
 az account set --subscription "0aa6e6f6-6e44-47f7-b30d-2aa0dfd4e5f4"
 
 # ⚠️ Replace RG and NAME with actual values from `az aks list`
-az aks get-credentials --resource-group RG --name roboshop
+az aks get-credentials --resource-group RG --name aks-cluster
 
 # ---------------------------
 # Verify cluster connection
